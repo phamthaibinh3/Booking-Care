493853a8157a15eebafcb69eff46259dfaaab964
Thai Binh <thaibinh100402@gmail.com>
2025-04-05 15:09:50 +0700

    CRUD user

---
+++ Node Js/src/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import viewEngine from './config/viewEngione';
+import inintWebRouter from './route/web';
+import connectDB from './config/connectDB'
+import cors from 'cors'
+
+require('dotenv').config();
+
+let app = express();
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+}));
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+viewEngine(app);
+inintWebRouter(app);
+
+connectDB();
+
+let port = process.env.PORT || 6969;
+app.listen(port, () => {
+    console.log("Backend Nodejs is runing on the port: " + port);
+})
