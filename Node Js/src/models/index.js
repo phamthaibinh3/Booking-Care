9da68ead6ca2975bf077775c955c4cccac013e24
Thai Binh <thaibinh100402@gmail.com>
2025-06-22 13:23:43 +0700

    Hoàn thiện backend chạy local

--- Node Js/src/models/index.js
+++ Node Js/src/models/index.js
@@ -4,16 +4,26 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const customizeConfig = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
+  logging: false,
+  query: {
+    "raw": true
+  },
+  timezone: "+07:00"
+};
+
+sequelize = new Sequelize(
+  process.env.DB_DATABASE_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  customizeConfig
+);
 
 fs
   .readdirSync(__dirname)
